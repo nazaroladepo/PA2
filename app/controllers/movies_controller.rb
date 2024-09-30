3fcfb675fdab2f66b519d48aeb2e9051534091b7
nazaro <nazaro@tamu.edu>
2024-09-30 02:23:17 -0500

    Styling and sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,69 +1,32 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
-
-  # GET /movies or /movies.json
-  def index
-    @movies = Movies.all
-  end
-
-  # GET /movies/1 or /movies/1.json
+\
   def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movies.find(id) # look up movie by unique ID
   end
 
   # GET /movies/new
   def new
-    @movie = Movie.new
+    @movie = Movies.new
   end
 
-  # GET /movies/1/edit
   def edit
+    @movie = Movies.find params[:id]
   end
 
-  # POST /movies or /movies.json
-  def create
-    @movie = Movie.new(movie_params)
 
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: "Movie was successfully created." }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
+  def index
+    sort_column = params[:sort] || session[:sort] || "title"
+    sort_direction = params[:direction] || session[:direction] || "asc"
+
+    session[:sort] = sort_column if params[:sort]
+    session[:direction] = sort_direction if params[:direction]
+
+    @movies = Movies.order("#{sort_column} #{sort_direction}")
   end
 
-  # PATCH/PUT /movies/1 or /movies/1.json
-  def update
-    respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: "Movie was successfully updated." }
-        format.json { render :show, status: :ok, location: @movie }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /movies/1 or /movies/1.json
-  def destroy
-    @movie.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to movies_path, status: :see_other, notice: "Movie was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,20 @@
 module MoviesHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == session[:sort] && session[:direction] == "asc") ? "desc" : "asc"
+\
+    link_to "#{title} #{sort_arrow(column)}".html_safe, { sort: column, direction: direction }
+  end
+
+  def sort_arrow(column)
+    return "" unless column == session[:sort]
+    session[:direction] == "asc" ? "▲" : "▼"
+  end
+
+
+  def highlight_column(column)
+    return "" unless column == params[:sort]
+
+    params[:direction] == "asc" ? "sorted-asc" : "sorted-desc"
+  end
 end
